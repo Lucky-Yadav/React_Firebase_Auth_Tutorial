62794090ad07e5208a9aada63bd0e6926e86f4b4
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-08-02 21:21:35 +0530

    update data

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: <YOURAPIKEY>,
-  authDomain: <YOURAUTHDOAMIN>,
-  projectId: <YOURPROJECTID>,
-  storageBucket: <YOURSTORAGEBUCKET>,
-  messagingSenderId: <YOURMESSAGINGSENDERID>,
-  appId: <YOURAPPID>,
+  apiKey: "AIzaSyAxKJozDZoGSmNPuB1rLmowf5zvweIgXIs",
+  authDomain: "subtle-well-328709.firebaseapp.com",
+  projectId: "subtle-well-328709",
+  storageBucket: "subtle-well-328709.appspot.com",
+  messagingSenderId: "40392879078",
+  appId: "1:40392879078:web:99f9a370aa3113cf77c427",
+  measurementId: "G-D95138J1LP",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
